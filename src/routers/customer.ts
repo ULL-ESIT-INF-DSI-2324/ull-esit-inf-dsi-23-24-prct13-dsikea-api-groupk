a5d63a23d548a06b61804f5164b009753e1f7747
Guille Díaz <alu0101505688@ull.edu.es>
2024-04-29 19:00:04 +0000

    added PATCH method

--- src/routers/customer.ts
+++ src/routers/customer.ts
@@ -12,6 +12,52 @@ customerRouter.post("/customers", (req, res) => {
   });
 });
 
+customerRouter.patch("/customers", (req, res) => {
+  const nif = req.query.nif;
+  if (nif) {
+    const allowedUpdates = [ "name", "surname", "telephoneNumber", "email", "address", "postalCode", "city", "gender" ];
+    const actualUpdates = Object.keys(req.body);
+    const isValidUpdate = actualUpdates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidUpdate) {
+      res.status(400).send("Update not permitted");
+    } else {
+      Customer.findOneAndUpdate({nif: nif}, req.body, {
+        new: true,
+        runValidators: true
+      }).then((customer) => {
+        if (!customer) res.status(404).send("Customer not found")
+        else res.status(200).send(customer);
+      }).catch((error) => {
+        res.status(400).send(error);
+      });
+    }
+  }
+  else res.status(400).send("Nif not provided")
+});
+
+customerRouter.patch("/customers/:id", (req, res) => {
+  const allowedUpdates = [ "name", "surname", "telephoneNumber", "email", "address", "postalCode", "city", "gender" ];
+  const actualUpdates = Object.keys(req.body);
+  const isValidUpdate = actualUpdates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidUpdate) {
+    res.status(400).send("Update not permitted");
+  } else {
+    Customer.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    }).then((customer) => {
+      if (!customer) res.status(404).send("Customer not found")
+      else res.status(200).send(customer);
+    }).catch((error) => {
+      res.status(400).send(error);
+    });
+  }
+});
+
+
+
 customerRouter.delete("/customers", (req, res) => {
   const nif = req.query.nif;
   if (nif){
