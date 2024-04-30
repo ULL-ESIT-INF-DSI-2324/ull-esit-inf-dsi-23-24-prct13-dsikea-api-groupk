26b169474b8fecc0ae01a1b7a473c4f52d84b6ae
Guille Díaz <alu0101505688@ull.edu.es>
2024-04-30 19:18:01 +0000

    added furniture router

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import "./db/mongoose.js";
 import { defaultRouter } from "./routers/default.js";
 import { customerRouter } from "./routers/customer.js";
 import { providerRouter } from "./routers/provider.js";
+import { furnitureRouter } from "./routers/furniture.js";
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(customerRouter);
 app.use(providerRouter);
+app.use(furnitureRouter);
 app.use(defaultRouter);
 
 app.listen(port, () => {
