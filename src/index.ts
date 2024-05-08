e630910ec71801f51465ece919e6db17fa756604
Guille Díaz <alu0101505688@ull.edu.es>
2024-05-08 18:10:24 +0000

    modified due to app.ts addition

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,6 @@
-import express from "express";
-import "./db/mongoose.js";
-import { defaultRouter } from "./routers/default.js";
-import { customerRouter } from "./routers/customer.js";
-import { providerRouter } from "./routers/provider.js";
-import { furnitureRouter } from "./routers/furniture.js";
-import { transactionRouter } from "./routers/transaction.js";
+import { app } from "./app.js"
 
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.json());
-
-/**
- * Instanciamos los routers
- */
-app.use(customerRouter);
-app.use(providerRouter);
-app.use(furnitureRouter);
-app.use(transactionRouter);
-app.use(defaultRouter);
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log("Server listening on", port);
